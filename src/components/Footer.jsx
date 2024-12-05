0a97ab27084156611f97cc01b7e5bb6e033edeac
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2024-12-05 15:59:42 +0800

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../styles.css'
+
+function Footer(){
+
+    const currentYear = new Date().getFullYear(); 
+    
+    return (
+        <>
+        <footer className="footer">
+            <p className="footer-text">
+            &copy; {currentYear} Moviedux, All rights reserved.</p>
+        </footer>
+        </>
+    )
+}
+
+export default Footer;
