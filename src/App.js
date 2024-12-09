72a426fb1f68edfdf1f1580fd31379c945eff2d3
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2024-12-09 15:37:13 +0800

    Update moviegrid component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
+import MovieGrid from './components/MoviesGrid';
 import './styles.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
         <div className="App">
             <div className="container">
                 <Header />
+                <MovieGrid />
             </div>
             <Footer />
         </div>
--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,12 +1,29 @@
-import React from "react";
-import '../styles.css'
+import React, { useEffect, useState } from "react";
+import "../styles.css";
 
-function MovieGrid(){
-    return(
-        <>
-        
-        </>
-    )
+function MovieGrid() {
+    const [movies, setMovie] = useState([]);
+
+    useEffect(() => {
+        fetch("movies.json")
+            .then((res) => res.json()) // convert movies.json into json
+            .then((res) => setMovie(res));
+    }, []);
+
+    return (
+        <div className="movies-grid">
+            {movies.map((movie) => (
+                <div key={movie.id} className="movie-card">
+                    <img src={`images/${movie.image}`} alt={movie.title} />
+                    <div className="movie-card-info">
+                        <h3 className="movie-card-title">{movie.title}</h3>
+                        <p className="movie-card-genre">{movie.genre}</p>
+                        <p className="movie-card-rating">{movie.rating}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default MovieGrid;
