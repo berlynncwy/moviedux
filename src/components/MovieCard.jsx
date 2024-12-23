084311947402969e8ef271898735dd7938892e41
berlynncwy <123068804+berlynncwy@users.noreply.github.com>
2024-12-23 14:16:22 +0800

    Add movieCard component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles.css";
+
+function MovieCard({ movie }) {
+    const handleError = (event) => {
+        event.target.src = "images/default.jpg";
+    };
+    return (
+        <div className="movie-card">
+            <img
+                src={`images/${movie.image}`}
+                alt={movie.title}
+                onError={handleError}
+            />
+            <div className="movie-card-info">
+                <h3 className="movie-card-title">{movie.title}</h3>
+                <p className="movie-card-genre">{movie.genre}</p>
+                <p className="movie-card-rating">{movie.rating}</p>
+            </div>
+        </div>
+    );
+}
+
+export default MovieCard;
